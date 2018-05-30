6fbd3669e0904418186925d8919989af23b4711b
gityii <419306244@qq.com>
2018-05-30 14:59:31 +0800

    gms 1.0

---
+++ advanced/common/models/Post.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "Post".
+ *
+ * @property int $id
+ * @property string $title
+ * @property string $content
+ * @property string $tags
+ * @property int $status
+ * @property int $create_time
+ * @property int $update_time
+ * @property int $author_id
+ */
+class Post extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'Post';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['title', 'content', 'status', 'author_id'], 'required'],
+            [['content', 'tags'], 'string'],
+            [['status', 'create_time', 'update_time', 'author_id'], 'integer'],
+            [['title'], 'string', 'max' => 128],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => '标题',
+            'content' => '内容',
+            'tags' => '标签',
+            'status' => '状态',
+            'create_time' => '创建时间',
+            'update_time' => '修改时间',
+            'author_id' => '作者',
+        ];
+    }
+
+
+    public function getAuthor()
+    {
+        return $this->hasOne(Adminuser::className(), ['id' => 'author_id']);
+    }
+
+    public function getStatus0()
+    {
+        return $this->hasOne(Poststatus::className(), ['id' => 'status']);
+    }
+
+    public function beforeSave($insert)
+    {
+        if(parent::beforeSave($insert))
+        {
+            if($insert)
+            {
+                $this->create_time = time();
+                $this->update_time = time();
+            }
+            else
+            {
+                $this->update_time = time();
+            }
+
+            return true;
+
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+}
